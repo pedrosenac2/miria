7db02b25d0c51000ae7500d9817511a768df866d
João Gabriel <jadezxfn25@gmail.com>
2024-02-20 17:09:50 -0300

    finish project organization

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,50 @@
+<?php
+
+if(str_contains($_SERVER['REQUEST_URI'], 'index')){
+    ?>
+    <div class="header">
+        
+    <div class="logo">
+        <span style="color: grey;">Miriã </span>
+        <span style="color: maroon;">Mauge</span>
+    </div>
+
+    <div class="navigation">
+        <a href="index.php">Home</a>
+        <a href="#">Contato</a>
+
+        <div class="dropdown">
+            <a href="#">Workshop</a>
+        </div>
+
+        <a href="pages/cursos.php">Cursos</a>
+        <a href="pages/e-books.php">E-books</a>
+        <a href="#">Dúvidas</a>
+    </div>
+</div>
+<?php
+}
+else{ ?>
 <div class="header">
         
         <div class="logo">
             <span style="color: grey;">Miriã </span>
             <span style="color: maroon;">Mauge</span>
         </div>
-
+    
         <div class="navigation">
-            <a href="index.html">Home</a>
+            <a href="../index.php">Home</a>
             <a href="#">Contato</a>
-
+    
             <div class="dropdown">
                 <a href="#">Workshop</a>
             </div>
-
-            <a href="cursos.html">Cursos</a>
-            <a href="#">E-books</a>
+    
+            <a href="../pages/cursos.php">Cursos</a>
+            <a href="../pages/e-books.php">E-books</a>
             <a href="#">Dúvidas</a>
-            <a href="#">Login</a>
         </div>
-</div>
+    </div>
+<?php
+}
+?>
