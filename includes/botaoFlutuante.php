dafd99d89bd54d63068cea6b9ffcb6ab4a89b38a
pedrosenac2 <pedrosenac02@gmail.com>
2024-02-23 16:08:42 -0300

    Create botaoFlutuante.php

---
+++ includes/botaoFlutuante.php
@@ -0,0 +1,33 @@
+<!-- Botão Flutuante -->
+<div class="fab">
+    <button class="main">
+    </button>
+    <ul>
+        <li>
+            <label for="curso">Cursos</label>
+            <button id="curso">
+                ⎈
+            </button>
+        </li>
+        <li>
+            <label for="ebook">E-books</label>
+            <button id="ebook">
+                ⎗
+            </button>
+        </li>
+        <li>
+            <label for="evento">Eventos</label>
+            <button id="evento">
+                ☏
+            </button>
+        </li>
+        <li>
+            <label for="contato">Contato</label>
+            <button id="contato">
+                ☏
+            </button>
+        </li>
+    </ul>
+</div>
+
+<!-- Fim Botão -->
