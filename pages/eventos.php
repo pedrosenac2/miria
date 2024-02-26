8e01cc360c3dd7e59319412491a88c8f7b0de246
pedrosenac2 <pedrosenac02@gmail.com>
2024-02-26 15:09:53 -0300

    add_evento_page
    ainda em contrução

---
+++ pages/eventos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/ebooks.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+
+
+    <title>E-books-Miriã Mauge</title>
+</head>
+
+<body>
+
+    <?php
+    include '../includes/botaoFlutuante.php'
+    ?>
+
+    <?php
+    include('../includes/header.php');
+    ?>
+
+    <div style="display: flex; justify-content: center;">
+        <img src="../assets/img/erro.png" alt="EmConstrução" style="width: 1000px;">
+    </div>
+
+
+    <?php
+    include('../includes/footer.php');
+    ?>
+
+    <script src="../assets/js/vermais.js"></script>
+    <script src="https://kit.fontawesome.com/2eeb56781b.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
+<!-- fim cabeçalho -->
