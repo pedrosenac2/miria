61cd4390d40fbf5c5813c19e77f5894d924d944b
pedrosenac2 <pedrosenac02@gmail.com>
2024-03-07 15:40:07 -0300

    painelADM
    adicionado Painel Administrativo

--- config/cursos-add.php
+++ config/cursos-add.php
@@ -1,20 +1,68 @@
 <?php
 include "conexao.php";
 
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     $name = $_POST['nome'];
-    $descricao = $_POST['descricao'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['nome'];
+    $desc = $_POST['descricao'];
 
-     $sql = "INSERT INTO tb_curso (nome, descricao) VALUES (:nome, :descricao)";
-     $stmt = $conn->prepare($sql); 
+    // Verifica se o arquivo foi enviado sem erros
+    if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
+        // Obtém o caminho do arquivo temporário
+        $imagem_temp = $_FILES['imagem']['tmp_name'];
 
-     $stmt->bindParam(':nome', $name);
-     $stmt->bindParam(':descricao', $descricao);
+        // Lê o conteúdo do arquivo
+        $imagem_data = file_get_contents($imagem_temp);
 
-    $stmt->execute();
+        // Insert na tb_img para ser recuperada na consulta abaixo
+        $sql = "INSERT INTO tb_img (img) VALUES (:img)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':img', $imagem_data, PDO::PARAM_LOB);
+        $stmt->execute();
 
-     header("Location: ../adm-test/admin-testc.html");
- }else{
-     echo "Acesso Inválido";
- }
+        // Pega o ID da última imagem inserida
+        $last_id = $conn->lastInsertId();
+
+        // Prepara a consulta SQL
+        $sql = "INSERT INTO tb_curso (nome, descricao, id_img) VALUES (:nome, :descricao, :id_img)";
+        $stmt = $conn->prepare($sql);
+
+        // Associa os parâmetros
+        $stmt->bindParam(':nome', $name);
+        $stmt->bindParam(':descricao', $desc);
+        $stmt->bindParam(':id_img', $last_id);
+
+        // Executa a consulta
+        $stmt->execute();
+
+        header("Location: ../PainelADM/index.php");
+
+        // Redireciona ou exibe uma mensagem de sucesso, conforme necessário
+    } else {
+        // Se houver um erro no upload do arquivo, exiba uma mensagem de erro
+        echo "Erro ao fazer upload da imagem.";
+    }
+} else {
+    echo "Acesso Inválido";
+}
+?>
+
+<?php
+// include "conexao.php";
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $name = $_POST['nome'];
+//     $desc = $_POST['descricao'];
+
+//     $sql = "INSERT INTO tb_ebooks (nome, descricao) VALUES (:nome, :descricao)";
+//     $stmt = $conn->prepare($sql); 
+
+//     $stmt->bindParam(':nome', $name);
+//     $stmt->bindParam(':descricao', $desc);
+
+//     $stmt->execute();
+
+//     header("Location: ../adm-test/admin-test.html");
+// }else{
+//     echo "Acesso Inválido";
+// }
 ?>
--- config/ebooks-add.php
+++ config/ebooks-add.php
@@ -4,29 +4,29 @@ include "conexao.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['nome'];
     $desc = $_POST['descricao'];
-    
+
     // Verifica se o arquivo foi enviado sem erros
     if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
         // Obtém o caminho do arquivo temporário
         $imagem_temp = $_FILES['imagem']['tmp_name'];
-        
+
         // Lê o conteúdo do arquivo
         $imagem_data = file_get_contents($imagem_temp);
-        
+
         // Prepara a consulta SQL
         $sql = "INSERT INTO tb_ebooks (nome, descricao, imagem) VALUES (:nome, :descricao, :imagem)";
-        $stmt = $conn->prepare($sql); 
-        
+        $stmt = $conn->prepare($sql);
+
         // Associa os parâmetros
         $stmt->bindParam(':nome', $name);
         $stmt->bindParam(':descricao', $desc);
         $stmt->bindParam(':imagem', $imagem_data, PDO::PARAM_LOB);
-        
+
         // Executa a consulta
         $stmt->execute();
 
-        header("Location: ../adm-test/admin-test.html");
-        
+        header("Location: ../PainelADM/index.php");
+
         // Redireciona ou exibe uma mensagem de sucesso, conforme necessário
     } else {
         // Se houver um erro no upload do arquivo, exiba uma mensagem de erro
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-
+<?php
 // include "conexao.php";
 
 // if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -56,3 +56,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // }else{
 //     echo "Acesso Inválido";
 // }
+
+?>
