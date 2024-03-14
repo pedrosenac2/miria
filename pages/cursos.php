586a8d503bd66c516c7fe0f776c6da450f2b6c9b
pedrosenac2 <pedrosenac02@gmail.com>
2024-03-14 14:45:45 -0300

    Update cursos.php

--- pages/cursos.php
+++ pages/cursos.php
@@ -12,10 +12,16 @@
 
 <body>
     <?php
+    // include '../includes/botaoFlutuante.php';
     include('../includes/header.php');
     include('../config/conexao.php');
 
-    $sql = "SELECT nome, descricao, id_img FROM tb_curso";
+    $courses = [];
+
+    $sql = "SELECT tb_curso.id ,tb_curso.nome, tb_curso.descricao, tb_curso.id_img, tb_img.img 
+    FROM tb_curso 
+    INNER JOIN tb_img 
+    ON tb_curso.id_img = tb_img.id";
     $resultado = $conn->query($sql);
 
     if ($resultado) {
@@ -48,19 +54,23 @@
                 <a class="link-curso" href="#?id=<?php echo $curso['id_img']; ?>">
                     <div class="curso">
                         <div class="img-curso">
-                            <img src="#<?php echo $curso['id_img']; ?>" alt="<?php echo $curso['nome']; ?>">
+                            <?php
+                            echo '<img class="card-img-top" alt="Card Imagem" src="data:image/jpeg;base64,' . base64_encode($curso['img']) . '" style="width:100%; max-height:300px; object-fit:cover; margin-bottom:15px;">';
+                            ?>
                         </div>
                         <div class="txt-curso">
-                            <h3 class="nome-curso"><?php echo $curso['nome']; ?></h3>
-                            <p><?php echo $curso['descricao']; ?></p>
+                            <h3 class="nome-curso">
+                                <?php echo $curso['nome']; ?>
+                            </h3>
+                            <p>
+                                <?php echo $curso['descricao']; ?>
+                            </p>
                         </div>
                     </div>
                 </a>
             <?php endforeach; ?>
         </div>
-        <?php if(count($cursos) > 1){ ?>   
-            <div class="button-container"><button class="veja-mais">Veja Mais</button></div>
-        <?php } ?>
+        <div class="button-container"><button class="veja-mais">Veja Mais</button></div>
     </div>
 
     <?php
@@ -72,8 +82,12 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
